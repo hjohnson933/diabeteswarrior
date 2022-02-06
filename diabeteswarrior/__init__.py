1e2beca346f39f31df1cce8b0dc89f3db19055e1
Harrison Johnson <hjohnson933@cox.net>
2022-02-06 07:46:27 -0600

    Start TDD

--- diabeteswarrior/__init__.py
+++ diabeteswarrior/__init__.py
@@ -1,2 +1,2 @@
 """Diabetes Management Application"""
-__version__ = '0.1.0'
+__version__:str = '0.1.0'
--- tests/test_diabeteswarrior.py
+++ tests/test_diabeteswarrior.py
@@ -1,5 +1,7 @@
+"""Tests for TDD"""
 from diabeteswarrior import __version__
 
 
 def test_version():
+    """Application version"""
     assert __version__ == '0.1.0'

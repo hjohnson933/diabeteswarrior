b54ce16e2e3ee906ecf1f7398dd8e3e211d22ee4
Harrison Johnson <hjohnson933@cox.net>
2022-02-06 10:33:50 -0600

    Working on health module init

--- tests/test_diabeteswarrior.py
+++ tests/test_diabeteswarrior.py
@@ -1,6 +1,14 @@
 """Tests for TDD"""
 from diabeteswarrior import __version__
+from diabeteswarrior.calc_insulin_dose import main as cid
+import diabeteswarrior.HLTH as Health
 
 def test_version():
     """Application version"""
     assert __version__ == '0.1.0'
+
+def test_insulin_dose():
+    """Test for a insulin dose"""
+    cid(act_bs=274) == "Bolus dose: 4, Basal dose: 2, Total dose: 6"
+
+

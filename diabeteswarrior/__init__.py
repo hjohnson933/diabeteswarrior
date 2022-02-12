6bce2c4ed17d89061d2ba95bf6770e039eeb7344
Harrison Johnson <hjohnson933@cox.net>
2022-02-11 20:17:30 -0600

    added bgl all test pass

--- diabeteswarrior/__init__.py
+++ diabeteswarrior/__init__.py
@@ -32,10 +32,11 @@ def create_app(test_config=None):
     db.init_app(app)
 
     # ? apply the blueprints to the app
-    from diabeteswarrior import auth, blog
+    from diabeteswarrior import auth, blog, bgl
 
     app.register_blueprint(auth.bp)
     app.register_blueprint(blog.bp)
+    app.register_blueprint(bgl.bp)
 
     # ? make url_for('index') == url_for('blog.index') in another app, you might define a separate main index here with app.route, while giving the blog blueprint a url_prefix, but for the tutorial the blog will be the main index
     app.add_url_rule("/", endpoint="index")

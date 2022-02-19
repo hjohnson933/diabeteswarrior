a5599bc6f218debf7c7b0c193bb06f7b845f2dec
Harrison Johnson <hjohnson933@cox.net>
2022-02-19 16:14:13 -0600

    Added models and reset site.db

--- diabeteswarrior/config.py
+++ diabeteswarrior/config.py
@@ -2,10 +2,10 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.cox.net'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
--- diabeteswarrior/models.py
+++ diabeteswarrior/models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from flask import current_app
 from flask_login import UserMixin
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous.jws import TimedJSONWebSignatureSerializer as Serializer
 
 from diabeteswarrior import db, login_manager
 
@@ -47,3 +47,84 @@ class Post(db.Model):
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
+
+
+class Health(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+    po_pulse = db.Column(db.Integer)
+    po_ox = db.Column(db.Integer)
+    weight = db.Column(db.Float)
+    fat = db.Column(db.Float)
+    bp_pulse = db.Column(db.Integer)
+    bp_systolic = db.Column(db.Integer)
+    bp_diastolic = db.Column(db.Integer)
+    bp_ihb = db.Column(db.Boolean)
+    bp_hypertension = db.Column(db.Integer)
+    temperature = db.Column(db.Float)
+
+
+class Scan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+    message = db.Column(db.Integer)
+    glucose_id = db.Column(db.Integer)
+    trend = db.Column(db.Integer)
+    notes = db.Column(db.Text)
+    bolus = db.Column(db.Boolean)
+    bolus_u = db.Column(db.Integer)
+    basal = db.Column(db.Boolean)
+    basal = db.Column(db.Integer)
+    food = db.Column(db.Boolean)
+    food_u = db.Column(db.Integer)
+    medication = db.Column(db.Boolean)
+    exercise = db.Column(db.Boolean)
+
+
+class Meal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+    servings = db.Column(db.Text)
+    indices = db.Column(db.Text)
+
+
+class Food(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+    domain = db.Column(db.String(120))
+    name = db.Column(db.String(120))
+    portion = db.Column(db.Integer)
+    unit = db.Column(db.String(120))
+    calories = db.Column(db.Integer)
+    fat = db.Column(db.Integer)
+    cholesterol = db.Column(db.Integer)
+    sodium = db.Column(db.Integer)
+    carbohydrate = db.Column(db.Integer)
+    protein = db.Column(db.Integer)
+
+
+class CGM(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+    Device = db.Column(db.String(120))
+    Number = db.Column(db.String(120))
+    Type = db.Column(db.Integer)
+    Scan = db.Column(db.Integer)
+    NonnumericRapidActingInsulin = db.Column(db.Boolean)
+    RapidActingInsulin = db.Column(db.Integer)
+    NonnumericFood = db.Column(db.Boolean)
+    CarbohydratesGrams = db.Column(db.Integer)
+    CarbohydratesServings = db.Column(db.Integer)
+    NonnumericLongActingInsulin = db.Column(db.Boolean)
+    LongActingInsulin = db.Column(db.Integer)
+    Notes = db.Column(db.Text)
+    Strip = db.Column(db.Integer)
+    Ketone = db.Column(db.Float)
+    MealInsulin = db.Column(db.Integer)
+    CorrectionInsulin = db.Column(db.Integer)
+    UserChangeInsulin = db.Column(db.Integer)
